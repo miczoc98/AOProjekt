9dbb5732d843f4a8895864ea1f7a9796070144ca
miczoc98 <miczoc98@gmail.com>
2021-01-23 16:53:19 +0100

    Added different binarization modes.

--- src/Binarizer.m
+++ src/Binarizer.m
@@ -1,12 +1,29 @@
-classdef Binarizer
+classdef Binarizer    
     properties
+        mode = 'manual';
+        isColorInversionOn = false;
         activeLayers = ActiveLayers();
         threshold = 0.5;
     end
     
     methods
         function binarized = binarize(this, image)
-            binarized = zeros(size(image, 1,2));
+            switch this.mode
+                case 'manual'
+                    binarized = manualBinarize(this, image);
+                case 'auto'
+                    binarized = autoBinarize(this, image);
+                case 'dynamic'
+                    binarized = dynamicBinarize(this, image);
+            end
+            
+            if this.isColorInversionOn
+                binarized = ~binarized;
+            end
+        end
+        
+        function binarized = manualBinarize(this, image)
+            binarized = zeros(size(image, 1, 2));
             
             if this.activeLayers.R == true
                 r = imbinarize(image(:,:,1), this.threshold);
@@ -20,8 +37,16 @@ classdef Binarizer
                 b = imbinarize(image(:,:,3), this.threshold);
                 binarized = binarized|b;
             end
-            
-            binarized = ~binarized;
+        end
+        
+        function binarized = autoBinarize(~, image)
+            image = rgb2gray(image);
+            binarized = imbinarize(image);
+        end
+        
+        function binarized = dynamicBinarize(~, image)
+            image = rgb2gray(image);
+            binarized = imbinarize(image, 'adaptive');
         end
     end
 end
